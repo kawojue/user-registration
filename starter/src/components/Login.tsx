782c17a3eb7b0735354ce4ec0b471bde8066b7b3
Raheem Kawojue <kawojue08@gmail.com>
2023-01-22 00:14:46 +0100

    [add] constraint container

--- starter/src/components/Login.tsx
+++ starter/src/components/Login.tsx
@@ -11,12 +11,14 @@ const Login:React.FC = () => {
                             <label htmlFor='username'>Username:</label>
                         </div>
                         <input type="text" id="username" />
+                        <div className='constraint'></div>
                     </div>
                     <div className="form-group">
                         <div className="validity-container">
                             <label htmlFor='pswd'>Password:</label>
                         </div>
                         <input type="password" id="pswd" />
+                        <div className='constraint'></div>
                     </div>
                 </article>
                 <div className="btn-container">
