a45a8b47ba9863f94a765784cab16ad997cedf9f
Raheem Kawojue <kawojue08@gmail.com>
2023-01-21 23:39:34 +0100

    [add] more structure

--- starter/src/components/Login.tsx
+++ starter/src/components/Login.tsx
@@ -2,18 +2,22 @@ const Login:React.FC = () => {
     return (
         <section className="container-center">
             <h3 className="section-h3">Login</h3>
-            <form>
-                <div className="form-group">
-                    <label htmlFor='username'>Username:</label>
-                    <input type="text" id="username" />
+            <form className="form">
+                <article className="form-center">
+                    <div className="form-group">
+                        <label htmlFor='username'>Username:</label>
+                        <input type="text" id="username" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor='pswd'>Password:</label>
+                        <input type="password" id="pswd" />
+                    </div>
+                </article>
+                <div className="btn-container">
+                    <button type="submit" className='btn' disabled={true}>
+                        Login
+                    </button>
                 </div>
-                <div className="form-group">
-                    <label htmlFor='pswd'>Password:</label>
-                    <input type="password" id="pswd" />
-                </div>
-                <button type="button" className='btn'>
-                    Login
-                </button>
             </form>
         </section>
     )
