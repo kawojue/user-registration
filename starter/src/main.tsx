6fe53eb116df2c5e069deb57bb155f86d8c85d56
Raheem Kawojue <kawojue08@gmail.com>
2023-01-22 00:10:57 +0100

    [add] router

--- starter/src/main.tsx
+++ starter/src/main.tsx
@@ -2,9 +2,14 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
