838fb4d3a0212adcfeaa588a38c1e36634eea126
Raheem Kawojue <kawojue08@gmail.com>
2023-01-22 00:11:36 +0100

    [add] routes

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -1,12 +1,15 @@
 import Login from "./components/Login"
 import Register from "./components/Register"
+import { Routes, Route } from "react-router-dom"
 
 const App: React.FC = () => {
   return (
-    <main className="container">
-      <Register/>
-      {/* <Login/> */}
-    </main>
+  <main className="container">
+    <Routes>
+      <Route index element={<Register/>} />
+      <Route path="/login" element={<Login/>} />
+    </Routes>
+  </main>
   )
 }
 
