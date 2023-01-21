63494943ecd6c8f63ef4bb5e1035215fec6dfec9
Raheem Kawojue <kawojue08@gmail.com>
2023-01-21 23:39:24 +0100

    [add] more structure

--- starter/src/components/Register.tsx
+++ starter/src/components/Register.tsx
@@ -2,22 +2,26 @@ const Register:React.FC = () => {
     return (
         <section className="container-center">
             <h3 className="section-h3">Register</h3>
-            <form>
-                <div className="form-group">
-                    <label htmlFor='username'>Username:</label>
-                    <input type="text" id="username" />
+            <form className="form">
+                <article className="form-center">
+                    <div className="form-group">
+                        <label htmlFor='username'>Username:</label>
+                        <input type="text" id="username" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor='pswd'>Password:</label>
+                        <input type="password" id="pswd" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor='confirm-pswd'>Confirm Password:</label>
+                        <input type="password" id="confirm-pswd" />
+                    </div>
+                </article>
+                <div className="btn-container">
+                    <button type="submit" className='btn'>
+                        Sign Up
+                    </button>
                 </div>
-                <div className="form-group">
-                    <label htmlFor='pswd'>Password:</label>
-                    <input type="password" id="pswd" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor='confirm-pswd'>Password:</label>
-                    <input type="password" id="confirm-pswd" />
-                </div>
-                <button type="button" className='btn'>
-                    Sign Up
-                </button>
             </form>
         </section>
     )
