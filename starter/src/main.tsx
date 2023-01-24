7649f6e34ec88c5ef26e76c8ebc8ee426f990b37
Raheem Kawojue <kawojue08@gmail.com>
2023-01-24 14:37:07 +0100

    [add] context provider

--- starter/src/main.tsx
+++ starter/src/main.tsx
@@ -2,14 +2,17 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { DataProvider } from './Context'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App/>}/>
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+    <DataProvider>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App/>}/>
+        </Routes>
+      </Router>
+    </DataProvider>
+  </React.StrictMode>
 )
