f313f3868994325d6aedba512c005fdcb3f4c5cd
Raheem Kawojue <kawojue08@gmail.com>
2023-01-24 14:37:40 +0100

    [edit] refractored

--- starter/src/components/Register.tsx
+++ starter/src/components/Register.tsx
@@ -1,11 +1,7 @@
-import { Link } from 'react-router-dom'
-import { useRef, useState, useEffect } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faCheck, faTimes, faInfoCircle} from '@fortawesome/free-solid-svg-icons'
+import userContext from "../Context"
 
 const Register: React.FC = () => {
-    const USER_REGEX:RegExp = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
-    const PSWD_REGEX:RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+    const { Link } = userContext()
     
     return (
         <section className="container-center">
