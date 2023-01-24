c9a506586f7c3dedb7b8507b2b5743a41c8008a7
Raheem Kawojue <kawojue08@gmail.com>
2023-01-24 21:32:04 +0100

    [add] username validation

--- starter/src/components/Register.tsx
+++ starter/src/components/Register.tsx
@@ -1,19 +1,45 @@
 import userContext from "../Context"
 
 const Register: React.FC = () => {
-    const { Link } = userContext()
-    
+    const {
+        Link, errMsg, errRef,
+        user, pswd, confirmPswd, userRef,
+        setUser, setPswd, setConfirmPswd,
+        setUserFocus, validName, userFocus,
+        FaInfoCircle, FaTimes, FaCheck
+    } = userContext()
+
     return (
         <section className="container-center">
+            <p ref={errRef} className={errMsg ? 'errMsg' : 'offscreen'} aria-live="assertive">
+                {errMsg}
+            </p>
             <h3 className="section-h3">Register</h3>
-            <form className="form">
+            <form className="form" action="" method="POST">
                 <article className="form-center">
                     <div className="form-group">
-                        <div className="validity-container">
-                            <label htmlFor='username'>Username:</label>
-                        </div>
-                        <input type="text" id="username" name="username" />
-                        <div className='constraint'></div>
+                        <article className="validity-container">
+                            <label htmlFor='username'>
+                                Username:
+                            </label>
+                            <div>
+                                <FaCheck className={validName ? 'valid': 'hidden'} />
+                                <FaTimes className={validName || !user ? 'hidden': 'invalid'} />
+                            </div>
+                        </article>
+                        <input type="text" id="username" name="username" autoComplete="off"
+                            ref={userRef} value={user} onChange={e => setUser(e.target.value)}
+                            onBlur={() => setUserFocus(false)} onFocus={() => setUserFocus(true)}
+                            aria-invalid={validName ? "false" : "true"} aria-describedby="uidnote" />
+                        <article
+                        className={userFocus && user && !validName ? 'constraint': 'hidden'}>
+                            <FaInfoCircle />
+                            <p id="uidnote">
+                                4 to 23 characters. <br />
+                                Must begin with a letter. <br />
+                                Symbols are allowed.
+                            </p>
+                        </article>
                     </div>
                     <div className="form-group">
                         <div className="validity-container">
