e7ed2e4d6e4bd0f54ed50722bccde54331be3970
Raheem Kawojue <kawojue08@gmail.com>
2023-01-26 14:01:40 +0100

    [add] register context

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -1,13 +1,18 @@
 import Login from "./components/Login"
 import Register from "./components/Register"
 import { Routes, Route } from "react-router-dom"
+import { RegisterProvider } from "./RegisterContext"
 
 const App: React.FC = () => {
   return (
   <main className="container">
     <Routes>
-      <Route index element={<Register/>} />
-      <Route path="/login" element={<Login/>} />
+        <Route index element={
+          <RegisterProvider>
+            <Register/>
+          </RegisterProvider>
+        } />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </main>
   )
