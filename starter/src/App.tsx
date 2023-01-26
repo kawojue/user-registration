fac2e0f373465f982d84e5d402d4c4fc41535a69
Raheem Kawojue <kawojue08@gmail.com>
2023-01-26 14:16:50 +0100

    [edit]

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -1,7 +1,7 @@
 import Login from "./components/Login"
 import Register from "./components/Register"
 import { Routes, Route } from "react-router-dom"
-import { RegisterProvider } from "./RegisterContext"
+import { RegisterProvider } from "./hooks/RegisterContext"
 
 const App: React.FC = () => {
   return (
