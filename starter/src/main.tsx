71e9e37935ae8d95b163cff571cdbc7a84059b1e
Raheem Kawojue <kawojue08@gmail.com>
2023-01-26 13:59:59 +0100

    refractored

--- starter/src/main.tsx
+++ starter/src/main.tsx
@@ -2,17 +2,17 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { DataProvider } from './Context'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { UserProvider } from './Context'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <DataProvider>
+    <UserProvider>
       <Router>
         <Routes>
-          <Route path="/*" element={<App/>}/>
+          <Route path="/*" element={<App />}/>
         </Routes>
       </Router>
-    </DataProvider>
+    </UserProvider>
   </React.StrictMode>
 )
