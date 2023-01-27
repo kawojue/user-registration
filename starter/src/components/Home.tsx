16c10fd9d6eaf5568f8d25514166441250f91cc4
Raheem Kawojue <kawojue08@gmail.com>
2023-01-27 17:52:55 +0100

    [add] navbar

--- starter/src/components/Home.tsx
+++ starter/src/components/Home.tsx
@@ -1,8 +1,26 @@
+import userContext from "../hooks/Context"
+
 const Home: React.FC = () => {
+    const { Link } = userContext()
+
     return (
-        <>
-            Our Homepage!
-        </>
+        <main className="homepage">
+            <nav className="navbar">
+                <article className="nav-center">
+                    <div>
+                        <p>Homepage</p>
+                    </div>
+                    <ul>
+                        <li>
+                            <Link to="/signup">Signup</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </article>
+            </nav>
+        </main>
     )
 }
 
