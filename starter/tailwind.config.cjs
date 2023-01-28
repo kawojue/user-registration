0997403bf1afd184157db182f878a7a78dc339a6
Raheem Kawojue <kawojue08@gmail.com>
2023-01-28 01:34:25 +0100

    [add] more colors

--- starter/tailwind.config.cjs
+++ starter/tailwind.config.cjs
@@ -9,8 +9,16 @@ module.exports = {
         "roboto": ['Roboto', 'sans-serif'],
       },
       colors: {
-        'pry-clr-0': "rgb(240, 245, 248)",
-        'pry-clr-1': "rgb(16, 16, 16)"
+        'pry-clr-0': 'rgb(241, 245, 248)',
+        'pry-clr-1': 'rgb(6, 50, 81)',
+        'pry-clr-2': 'rgb(97, 125, 152)',
+        'pry-clr-3': 'rgb(104,182,238)',
+        'pry-clr-4': 'rgb(73, 166, 233)',
+        'pry-clr-5': '#ff7f50',
+        'pry-clr-6': '#e66b6b',
+        'pry-clr-7': '#bb2525',
+        'pry-clr-8': '#6be675',
+        'pry-clr-9': '#25bb32',
       }
     },
   },
