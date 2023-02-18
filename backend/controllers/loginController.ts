925cf17c99fa12fe203082acf38e0c496c678afa
Raheem Kawojue <kawojue08@gmail.com>
2023-02-18 20:43:15 +0100

    [add] login controller

---
+++ backend/controllers/loginController.ts
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv'
+import * as bcrypt from 'bcrypt'
+import User from '../model/userSchema'
+import jwt, { Secret } from 'jsonwebtoken'
+import { Request, Response } from 'express'
+
+dotenv.config()
+
+export const handleLogin = async (req: Request, res: Response) => {
+    const { user, pswd } = req.body
+    if (!user || !pswd) return res.sendStatus(400) // bad request
+    const existingUser = await User.findOne({ username: user }).exec()
+    if (!existingUser) return res.sendStatus(409) // user does not exist - conflict
+
+    try {
+        const checkPswd = await bcrypt.compare(pswd, existingUser.password)
+        if (!checkPswd) return res.sendStatus(401) // incorrect password - unauthorized
+        const accessToken: Secret = jwt.sign(
+            { "username": user },
+            `${process.env.SECRET_ACCESS_TOKEN}`,
+            { expiresIn: '1min' }
+        )
+        const refreshToken: Secret = jwt.sign(
+            { "username": user },
+            `${process.env.SECRET_REFRESH_TOKEN}`,
+            { expiresIn: '1d' }
+        )
+
+        existingUser.refreshToken = refreshToken
+        await existingUser.save()
+        res.cookie('loginCookie', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+        return res.status(200).json({
+            success: true,
+            message: accessToken
+        })
+    } catch(err: any) {
+        res.sendStatus(500)
+    }
+}
