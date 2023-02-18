20f1a09f07b9efd38b16ff3db2d32b3006124927
Raheem Kawojue <kawojue08@gmail.com>
2023-02-19 00:22:19 +0100

    [add] refresh token controller

---
+++ backend/controllers/refreshTokenController.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv'
+import User from '../model/userSchema'
+import jwt, { Secret } from 'jsonwebtoken'
+import { Request, Response } from 'express'
+
+dotenv.config()
+
+export const handleRefreshToken = async (req: Request, res: Response) => {
+    const cookies = req.cookies
+    if (!cookies?.loginCookie) return res.sendStatus(401) // unauthorized
+
+    const refreshToken = cookies.loginCookies
+    const existingUser = await User.findOne({ refreshToken }).exec()
+
+    if (!existingUser) return res.sendStatus(403) // forbidden
+
+    jwt.verify(
+        refreshToken,
+        `${process.env.SECRET_REFRESH_TOKEN}`,
+        (err: any, decoded: any) => {
+            if (err || decoded.username !== existingUser.username) return res.sendStatus(403)
+            const accessToken: Secret = jwt.sign(
+                { "username": decoded.username },
+                `${process.env.SECRET_ACCESS_TOKEN}`,
+                { expiresIn: '5m'}
+            )
+            res.status(200).json({ success: true, message: accessToken })
+        }
+    )
+
+}
