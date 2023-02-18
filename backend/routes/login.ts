8666d82e0a119d9ce6a96b773ed5f79fe0175e10
Raheem Kawojue <kawojue08@gmail.com>
2023-02-18 23:24:52 +0100

    .

--- backend/routes/login.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { handleLogin } from '../controllers/loginController'
-
-const router = express.Router()
-
-router.post('/login', handleLogin)
-
-export default router
--- backend/routes/signup.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { handleSignup } from '../controllers/signupController'
-
-const router = express.Router()
-
-router.post('/signup', handleSignup)
-
-export default router
