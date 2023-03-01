95c49f45e3d162f8682c6b875cf089cd4b065f0b
Raheem Kawojue <kawojue08@gmail.com>
2023-03-02 00:56:49 +0100

    [fix] loophole

--- backend/controllers/signupController.ts
+++ backend/controllers/signupController.ts
@@ -4,15 +4,17 @@ import { Request, Response } from 'express'
 
 export const handleSignup = async (req: Request, res: Response) => {
     const { user, pswd } = req.body
-    const existingUser = await User.findOne({ username: user }).exec()
+    const username = user.toLowerCase()
 
-    if (!user || !pswd) return res.sendStatus(400)
+    const existingUser = await User.findOne({ username }).exec()
+
+    if (!username || !pswd) return res.sendStatus(400)
     if (existingUser) return res.sendStatus(409)
 
     try {
         const hashedPswd = await bcrypt.hash(pswd, 12)
         await User.create({
-            username: user,
+            username,
             password: hashedPswd
         })
         return res.sendStatus(201)
