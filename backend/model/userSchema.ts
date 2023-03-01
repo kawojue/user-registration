0361c7dba7123252ef9832a5a7427c5827c00865
Raheem Kawojue <kawojue08@gmail.com>
2023-03-02 00:57:06 +0100

    [add] timestamps

--- backend/model/userSchema.ts
+++ backend/model/userSchema.ts
@@ -1,15 +1,27 @@
 import * as mongoose from 'mongoose'
+const autoincrement = require('mongoose-sequence')(mongoose)
 
-const userShema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
+const userShema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        refreshToken: String
     },
-    password: {
-        type: String,
-        required: true
-    },
-    refreshToken: String
+    {
+        timestamps: true
+    }
+)
+
+userShema.plugin(autoincrement, {
+    inc_field: 'user',
+    id: 'userNums',
+    start_req: 1
 })
 
 export default mongoose.model('userModel', userShema)
