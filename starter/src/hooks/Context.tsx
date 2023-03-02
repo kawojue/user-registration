35e04c93a01c6f874bc7b9588d5d9e5184a8eb79
Raheem Kawojue <kawojue08@gmail.com>
2023-03-02 01:22:17 +0100

    clr credentials

--- starter/src/hooks/Context.tsx
+++ starter/src/hooks/Context.tsx
@@ -78,6 +78,9 @@ export const UserProvider: React.FC<{ children: React.ReactElement }> = ({ child
         })
 
         if (res) {
+            setUser("")
+            setPswd("")
+            setConfirmPswd("")
             setSuccess(true)
         } else {
             setTimeout(() => {
