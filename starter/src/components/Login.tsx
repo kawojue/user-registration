23ad39c2812408c5f0bf23f948dc17314ed52d49
Raheem Kawojue <kawojue08@gmail.com>
2023-03-02 18:08:56 +0100

    [add] error ref

--- starter/src/components/Login.tsx
+++ starter/src/components/Login.tsx
@@ -11,39 +11,50 @@ const Login:React.FC = () => {
     } = userContext()
 
     const errRef = useRef<any>()
-    const userRef = useRef<HTMLInputElement>()
+    const userRef = useRef<HTMLInputElement>(null)
 
     const [user, setUser] = useState<string>("")
     const [pswd, setPswd] = useState<string>("")
     const [errMsg, setErrMsg] = useState<string>("")
     const [success, setSuccess] = useState<boolean>(false)
 
+    const isValid = Boolean(user) && Boolean(pswd)
+
     useEffect(() => {
         userRef.current?.focus()
     }, [])
+
+    useEffect(() => {
+        setErrMsg("")
+    }, [user, pswd])
     
     return (
         <section className="container-center">
+            <div className={`err-container ${errMsg ? 'errMsg' : 'hidden'}`}>
+                    <p ref={errRef} aria-live="assertive">
+                        {errMsg}
+                    </p>
+            </div>
             <h3 className="section-h3">Login</h3>
             <form className="form">
                 <article className="form-center">
                     <div className="form-group">
-                        <div className="validity-container">
-                            <label htmlFor='username'>Username:</label>
-                        </div>
-                        <input type="text" id="username" name="username" />
+                        <label htmlFor='username'>Username:</label>
+                        <input type="text" id="username" name="username" ref={userRef}
+                        value={user} onChange={e => setUser(e.target.value)}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor='pswd'>Password:</label>
                         <article className="pswd-container">
                             <input type={showPswd ? 'text': 'password'}
-                            id="pswd" name="password" />
+                            id="pswd" name="password" value={pswd}
+                            onChange={e => setPswd(e.target.value)} />
                             <Button get={showPswd} set={setShowPswd} />
                         </article>
                     </div>
                 </article>
                 <div className="btn-container">
-                    <button type="submit" className='btn'>
+                    <button type="submit" className='btn' disabled={!isValid}>
                         Login
                     </button>
                 </div>
