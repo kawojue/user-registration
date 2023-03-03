9ab49b3b4bd8fd6273104ccab748883b85d5f8a0
Raheem Kawojue <kawojue08@gmail.com>
2023-03-03 14:21:43 +0100

    .

--- backend/middlewares/errorHandler.ts
+++
@@ -1,6 +0,0 @@
-import { ErrorRequestHandler, Request, Response, NextFunction} from 'express'
-
-export default function errorHandler (err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) {
-    console.error(err)
-    res.status(500).json({ success: false, message: 'Server Error.' })
-}
