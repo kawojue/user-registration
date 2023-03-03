96523f8397b047a3c4be8c19bdf05f199103c629
Raheem Kawojue <kawojue08@gmail.com>
2023-03-03 13:16:18 +0100

    [add] roles config

---
+++ backend/config/roles.ts
@@ -0,0 +1,13 @@
+export interface IRoles {
+    Employee?: Number,
+    Admin?: Number,
+    User: Number
+}
+
+const roles: IRoles = {
+    Employee: 2384,
+    User: 9823,
+    Admin: 4589
+}
+
+export default roles
