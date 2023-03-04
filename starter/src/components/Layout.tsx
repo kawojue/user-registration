55565af1a1fd4c354d0f823aeef636dac5159e59
Raheem Kawojue <kawojue08@gmail.com>
2023-03-04 14:06:47 +0100

    refractored

--- starter/src/components/Layout.tsx
+++ starter/src/components/Layout.tsx
@@ -1,8 +1,23 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 const Layout: React.FC = () => {
     return (
         <main className="app">
+            <nav className="navbar">
+                <article className="nav-center">
+                    <div>
+                        <p>Homepage</p>
+                    </div>
+                    <ul>
+                        <li>
+                            <Link to="/auth/signup">Signup</Link>
+                        </li>
+                        <li>
+                            <Link to="/auth/login">Login</Link>
+                        </li>
+                    </ul>
+                </article>
+            </nav>
             <Outlet />
         </main>
     )
