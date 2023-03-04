bb43e4e1009a8102d980f8fb5c83ddf5bf96edd1
Raheem Kawojue <kawojue08@gmail.com>
2023-03-04 14:06:41 +0100

    refractored

--- starter/src/components/Home.tsx
+++ starter/src/components/Home.tsx
@@ -1,28 +1,7 @@
-import userContext from "../hooks/Context"
-
 const Home: React.FC = () => {
-    const { Link } = userContext()
-
     return (
-        <main>
-            <nav className="navbar">
-                <article className="nav-center">
-                    <div>
-                        <p>Homepage</p>
-                    </div>
-                    <ul>
-                        <li>
-                            <Link to="/auth/signup">Signup</Link>
-                        </li>
-                        <li>
-                            <Link to="/auth/login">Login</Link>
-                        </li>
-                    </ul>
-                </article>
-            </nav>
-            <article className="container">
-
-            </article>
+        <main className="container">
+            <p>Welcome!</p>
         </main>
     )
 }
