3c4905e7539d6d40f1155569a9aa8c8cc5fe60bb
Raheem Kawojue <kawojue08@gmail.com>
2023-03-05 14:35:28 +0100

    [add] dashboard component

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -3,12 +3,14 @@ import Login from "./components/Login"
 import Layout from "./components/Layout"
 import Missing from "./components/Missing"
 import Register from "./components/Register"
+import Dashboard from "./components/Dashboard"
 import { Routes, Route } from "react-router-dom"
 
 const App: React.FC = () => {
   return (
     <>
     <Routes>
+      <Route path="/:id" element={<Dashboard />} />
       <Route path="/" element={<Layout />}>
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/signup" element={<Register />} />
