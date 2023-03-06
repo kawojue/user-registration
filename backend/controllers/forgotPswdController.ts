43e79683efbae2db60a237d02df3181bba4b044a
Raheem Kawojue <kawojue08@gmail.com>
2023-03-06 02:26:11 +0100

    [add] forgot pswd controller

---
+++ backend/controllers/forgotPswdController.ts
@@ -0,0 +1,32 @@
+import speakeasy from 'speakeasy'
+import nodemailer from 'nodemailer'
+import User from '../model/userSchema'
+import { Request, Response } from 'express'
+const asyncHandler = require('express-async-handler')
+
+export const handleForgotPswd = asyncHandler(async (req: Request, res: Response) => {
+    const { email } = req.body
+    const mail: string = email?.toLowerCase()
+
+    console.log(mail)
+
+    const exists = await User.findOne({ email: mail }).exec()
+
+    if (!email) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid email address.'
+        })
+    }
+
+    if (!exists) {
+        return res.status(401).json({
+            success: false,
+            message: 'Email does not exist.'
+        })
+    }
+
+    res.status(200).json({
+        success: true
+    })
+})
