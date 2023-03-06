39da1195ceeb9e70c7f6f8f457f45f2b344d9713
Raheem Kawojue <kawojue08@gmail.com>
2023-03-06 02:24:36 +0100

    .

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -5,21 +5,23 @@ import Missing from "./components/Missing"
 import Register from "./components/Register"
 import Dashboard from "./components/Dashboard"
 import { Routes, Route } from "react-router-dom"
+import ForgotPswd from "./components/ForgotPswd"
 
 const App: React.FC = () => {
   return (
     <>
-    <Routes>
-      <Route path="/:id" element={<Dashboard />} />
-      <Route path="/" element={<Layout />}>
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/signup" element={<Register />} />
+      <Routes>
+        <Route path="/:id" element={<Dashboard />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/signup" element={<Register />} />
+          <Route path="/auth/forgotten" element={<ForgotPswd />} />
 
-        <Route index element={<Home />}/>
-        <Route path="*" element={<Missing />}/>
-      </Route>
-    </Routes>
-  </>
+          <Route index element={<Home />}/>
+          <Route path="*" element={<Missing />}/>
+        </Route>
+      </Routes>
+    </>
   )
 }
 
