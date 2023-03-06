4d25bad34cc3737dec9e136f46f7090cc75eb1a9
Raheem Kawojue <kawojue08@gmail.com>
2023-03-06 02:25:10 +0100

    [add] forgot pswd route

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -2,11 +2,13 @@ import express, { Router } from 'express'
 import { handleLogin } from '../controllers/loginController'
 import { handleLogout } from '../controllers/logoutController'
 import { handleSignup } from '../controllers/signupController'
+import { handleForgotPswd } from '../controllers/forgotPswdController'
 
 const authRoute: Router = express.Router()
 
 authRoute.post('/login', handleLogin)
 authRoute.get('/logout', handleLogout)
 authRoute.post('/signup', handleSignup)
+authRoute.post('/forgotten', handleForgotPswd)
 
 export default authRoute
