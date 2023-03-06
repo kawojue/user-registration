b1a1dee7999558c2b9e1f2cb03a88a3e646c5cd8
Raheem Kawojue <kawojue08@gmail.com>
2023-03-06 01:17:42 +0100

    [rem] port

--- backend/env.d.ts
+++ backend/env.d.ts
@@ -2,7 +2,6 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             DATABASE_URI?: string
-            PORT?: string
             SECRET_ACCESS_TOKEN?: string
             SECRET_REFRESH_TOKEN?: string
         }
