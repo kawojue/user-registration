c19427ad14b6f7829262097b841b11063e3e88e8
Raheem Kawojue <kawojue08@gmail.com>
2023-03-06 01:11:54 +0100

    optimization

--- starter/src/components/Dashboard.tsx
+++ starter/src/components/Dashboard.tsx
@@ -5,21 +5,29 @@ import { useParams, useNavigate } from 'react-router-dom'
 const Dashboard: React.FC = () => {
     const nav = useNavigate()
     const { id } = useParams()
+    const isValidParam = id?.startsWith('@')
     const userId: string | undefined = id?.slice(1)
-    const handleUser = async (ID: string | undefined):Promise<void> => {
-        await axios.get(`${ID}`)
-        .then(res => {
+
+    const handleUser = async (ID: string | undefined) => {
+        await axios.get(`${ID}`).then(res => {
             console.log(res?.data)
         }).catch(err => {
             if (err?.response.status === 404) {
                 nav('*')
+            } else {
+                console.log('Network error')
             }
         })
     }
 
     useEffect(() => {
-        (async () => await handleUser(userId))()
-    })
+        if (isValidParam) {
+            (async () => await handleUser(userId))()
+        } else {
+            nav(`/@${id}`)
+        }
+    }, [])
+
     return (
         <p>{id}</p>
     )
