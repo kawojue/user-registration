59eeb9c9d3e5ef6abac92bd2cd9211398f02a491
Raheem Kawojue <kawojue08@gmail.com>
2023-03-08 11:20:56 +0100

    refractored

--- backend/controllers/forgotPswdController.ts
+++ backend/controllers/forgotPswdController.ts
@@ -1,12 +1,14 @@
+import mailer from '../config/mailer'
 import User from '../model/userSchema'
 import { Request, Response } from 'express'
+import { manageOTP } from '../config/manageOTP'
 const asyncHandler = require('express-async-handler')
 
 export const handleForgotPswd = asyncHandler(async (req: Request, res: Response) => {
     const { email } = req.body
     const mail: string = email?.toLowerCase().trim()
 
-    const exists = await User.findOne({ 'mail.email': mail }).exec()
+    const exists: any = await User.findOne({ 'mail.email': mail }).exec()
 
     if (!email) {
         return res.status(400).json({
@@ -22,7 +24,11 @@ export const handleForgotPswd = asyncHandler(async (req: Request, res: Response)
         })
     }
 
+    const { totp, secret } = manageOTP()
+    await mailer('Always Appear', exists.mail?.email, 'Forgot Password', `Code: ${totp}`)
+
     res.status(200).json({
-        success: true
+        success: true,
+        secret
     })
 })
