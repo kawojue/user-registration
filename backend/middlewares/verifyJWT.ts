98022e6230d448889c56c7525f10dbcdb165fd6d
Raheem Kawojue <kawojue08@gmail.com>
2023-03-09 12:59:07 +0100

    [fix] token verification

--- backend/middlewares/verifyJWT.ts
+++ backend/middlewares/verifyJWT.ts
@@ -5,16 +5,18 @@ import { Request, Response, NextFunction } from 'express'
 dotenv.config()
 
 export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
-    const authHeader:any = req.headers[`${'a' || 'A'}'uthorization'`]
+    const authHeader:any = req.headers['authorization']
     if (!authHeader?.startsWith('Bearer')) return res.sendStatus(401)
     
-    const token = authHeader?.split('')[1]
+    const token = authHeader?.split(' ')[1]
     jwt.verify(
         token,
         `${process.env.SECRET_ACCESS_TOKEN}`,
-        (err: any, decoded:any) => {
+        (err: any, user:any) => {
             if (err) return res.sendStatus(403)
-            req.body.user = decoded.username
+            // console.log(user)
+            // console.log(req)
+            // req.user = user.userInfo.userId
             next()
         }
     )
