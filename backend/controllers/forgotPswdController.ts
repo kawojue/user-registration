604cbc3c9e32d8774e57e0594d4f9e2cbb9191fc
Raheem Kawojue <kawojue08@gmail.com>
2023-03-11 13:38:39 +0100

    [fix] forgot pswd

--- backend/controllers/forgotPswdController.ts
+++ backend/controllers/forgotPswdController.ts
@@ -1,9 +1,12 @@
 import User from '../model/userSchema'
+import mailer from '../config/mailer'
 import { Request, Response } from 'express'
+import { generateOTP } from '../config/manageOTP'
 const asyncHandler = require('express-async-handler')
 
 export const handleForgotPswd = asyncHandler(async (req: Request, res: Response) => {
     const { email } = req.body
+    const otp = generateOTP()
     const mail: string = email?.toLowerCase().trim()
 
     const exists: any = await User.findOne({ 'mail.email': mail }).exec()
@@ -22,7 +25,10 @@ export const handleForgotPswd = asyncHandler(async (req: Request, res: Response)
         })
     }
 
+    await mailer("Always Appear", mail, "Forgot Password", `Code: ${otp}`)
+
     res.status(200).json({
         success: true,
+        totp: otp
     })
 })
