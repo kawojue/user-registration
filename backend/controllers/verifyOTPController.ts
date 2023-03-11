805679b4b273456a2e8049cbebde7555e11c72b1
Raheem Kawojue <kawojue08@gmail.com>
2023-03-11 14:43:47 +0100

    [add] otp expiry

--- backend/controllers/verifyOTPController.ts
+++ backend/controllers/verifyOTPController.ts
@@ -2,9 +2,17 @@ import { Request, Response } from 'express'
 const asyncHandler = require('express-async-handler')
 
 export const verify = asyncHandler(async (req: Request, res: Response) => {
-    let { userId, otp, totp } = req.body
+    let { userId, otp, totp, date }: any = req.body
     otp = otp?.trim()
     userId = userId?.trim().toLowerCase()
+
+    const expiry:number = date + 25*1000
+
+    if (expiry < Date.now()) {
+        return res.status(400).json({
+            message: "OTP Expired."
+        })
+    }
 
     if (totp !== otp) {
         return res.status(401).json({
