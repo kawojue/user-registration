122a193d9f57aa77c3cc4fde6851c34260bb1423
Raheem Kawojue <kawojue08@gmail.com>
2023-03-12 00:02:43 +0100

    [add] passwordreset route

--- backend/routes/accountRoutes.ts
+++ backend/routes/accountRoutes.ts
@@ -1,7 +1,10 @@
 import express, { Router } from 'express'
+import { verify } from '../controllers/verifyOTPController'
+import { handleChangePswd } from '../controllers/changePswdController'
 
 const router: Router = express.Router()
 
-router.post('/reset')
+router.post('/reset', verify)
+router.post('/passwordreset', handleChangePswd)
 
 export default router
