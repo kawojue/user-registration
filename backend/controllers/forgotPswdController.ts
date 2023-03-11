8807c6ac3704eb9a0ab9a59e6b52e5668ac9c991
Raheem Kawojue <kawojue08@gmail.com>
2023-03-11 14:44:06 +0100

    [add] otp expiry date

--- backend/controllers/forgotPswdController.ts
+++ backend/controllers/forgotPswdController.ts
@@ -1,12 +1,12 @@
-import User from '../model/userSchema'
 import mailer from '../config/mailer'
+import User from '../model/userSchema'
 import { Request, Response } from 'express'
-import { generateOTP } from '../config/manageOTP'
+import { generateOTP, IGenOTP } from '../config/manageOTP'
 const asyncHandler = require('express-async-handler')
 
 export const handleForgotPswd = asyncHandler(async (req: Request, res: Response) => {
     const { email } = req.body
-    const otp = generateOTP()
+    const { OTP, now }: IGenOTP = generateOTP()
     const mail: string = email?.toLowerCase().trim()
 
     const exists: any = await User.findOne({ 'mail.email': mail }).exec()
@@ -25,10 +25,11 @@ export const handleForgotPswd = asyncHandler(async (req: Request, res: Response)
         })
     }
 
-    await mailer("Always Appear", mail, "Forgot Password", `Code: ${otp}`)
+    await mailer("Always Appear", mail, "Forgot Password", `Code: ${OTP}`)
 
     res.status(200).json({
         success: true,
-        totp: otp
+        totp: OTP,
+        date: now
     })
 })
