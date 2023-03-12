569cb113bc71294f4d5a6c0b8c86100921f74017
Raheem Kawojue <kawojue08@gmail.com>
2023-03-12 14:49:57 +0100

    [change] route

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -2,7 +2,6 @@ import express, { Router } from 'express'
 import { handleLogin } from '../controllers/loginController'
 import { handleLogout } from '../controllers/logoutController'
 import { handleSignup } from '../controllers/signupController'
-import { handleForgotPswd } from '../controllers/forgotPswdController'
 import { handleRefreshToken } from '../controllers/refreshTokenController'
 
 const authRoute: Router = express.Router()
@@ -11,6 +10,5 @@ authRoute.post('/login', handleLogin)
 authRoute.get('/logout', handleLogout)
 authRoute.post('/signup', handleSignup)
 authRoute.get('/refresh', handleRefreshToken)
-authRoute.post('/forgotten', handleForgotPswd)
 
 export default authRoute
