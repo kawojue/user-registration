faa05c9e26bc4f8591190b7535593aea493cf8fb
Raheem Kawojue <kawojue08@gmail.com>
2023-03-15 10:59:40 +0100

    .

--- backend/middlewares/verifyRoles.ts
+++ backend/middlewares/verifyRoles.ts
@@ -1,11 +1,11 @@
-import { Request, Response, NextFunction } from 'express'
+import { Response, NextFunction } from 'express'
 
 const verifyRoles = (...allowedRoles: number[]) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        if (!req?.body?.roles) return res.sendStatus(401)
+    return (req: any, res: Response, next: NextFunction) => {
+        if (!req?.roles) return res.sendStatus(401)
 
         const rolesArray: number[] = [...allowedRoles]
-        const re: boolean[] = req?.body?.roles.map((role: number) => rolesArray.includes(role)).find((val: boolean) => val === true)
+        const re: boolean[] = req?.roles.map((role: number) => rolesArray.includes(role)).find((val: boolean) => val === true)
         if (!re) return res.sendStatus(401)
         next()
     }
