a6069d8c8981ef7873c8739e5c0e85d819f622f2
Raheem Kawojue <kawojue08@gmail.com>
2023-03-22 13:27:20 +0100

    [add] login limiter middleware

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express'
-import loginLimiter from '../middlewares/limiter'
+import limiter from '../middlewares/limiter'
 import { handleLogin } from '../controllers/loginController'
 import { handleLogout } from '../controllers/logoutController'
 import { handleSignup } from '../controllers/signupController'
@@ -10,6 +10,6 @@ const authRoute: Router = express.Router()
 authRoute.get('/logout', handleLogout)
 authRoute.post('/signup', handleSignup)
 authRoute.get('/refresh', handleRefreshToken)
-authRoute.post('/login', loginLimiter, handleLogin)
+authRoute.post('/login', limiter, handleLogin)
 
 export default authRoute
