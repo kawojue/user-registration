b2860c286d9b815c4d54df110402c045087ed1ce
Raheem Kawojue <kawojue08@gmail.com>
2023-03-22 15:25:04 +0100

    [add] origin

--- backend/config/allowedLists.ts
+++ backend/config/allowedLists.ts
@@ -1,5 +1,6 @@
 export const allowedLists: string[] = [
     'https://vercel.com',
     'http://localhost:5173',
-    'https://user-registration-gamma.vercel.app'
+    'https://user-registration-gamma.vercel.app',
+    'https://user-registration-kawojue.vercel.app'
 ]
