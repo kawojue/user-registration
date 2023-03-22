3909d3d3ba90865c6d1dc58c89152c275bac0c35
Raheem Kawojue <kawojue08@gmail.com>
2023-03-22 13:37:59 +0100

    [edit] otp length

--- backend/config/manageOTP.ts
+++ backend/config/manageOTP.ts
@@ -7,7 +7,7 @@ export function generateOTP(): IGenOTP {
     let OTP: string = ''
     const now: number = Date.now()
     const digits: string = '0123456789'
-    const length: number = parseInt('56'[Math.floor(Math.random() * 2)])
+    const length: number = parseInt('65'[Math.floor(Math.random() * 2)])
     for (let i = 0; i < length; i++) {
         OTP += digits[Math.floor(Math.random() * length)]
     }
