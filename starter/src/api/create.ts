a0a9aea83d283f05deca0382916737eaee5619ad
Raheem Kawojue <kawojue08@gmail.com>
2023-03-22 15:09:38 +0100

    [add] prod link

--- starter/src/api/create.ts
+++ starter/src/api/create.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const url: string = process.env.NODE_ENV === 'production' ? 'https://user-registration-gamma.vercel.app/' : 'http://localhost:2003'
+
 export default axios.create({
-    baseURL: 'http://localhost:2003'
+    baseURL: url
 })
