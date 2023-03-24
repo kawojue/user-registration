8f9a23a491f0c294f0fda90df84b924ed317dd50
Raheem Kawojue <kawojue08@gmail.com>
2023-03-24 14:08:39 +0100

    .

--- starter/src/api/create.ts
+++ starter/src/api/create.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url: string = process.env.NODE_ENV === 'production' ? 'https://user-registration-gamma.vercel.app/' : 'http://localhost:2003'
+const url: string = process.env.NODE_ENV === 'production' ? 'user-registration-backend-kawojue.vercel.app' : 'http://localhost:2003'
 
 export default axios.create({
     baseURL: url
