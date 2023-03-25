68c441d69ed4bf1de0abf3904061d6a0d9f99750
Raheem Kawojue <kawojue08@gmail.com>
2023-03-25 16:57:04 +0100

    [add] ...

--- starter/src/components/Home.tsx
+++ starter/src/components/Home.tsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+
 const Home: React.FC = () => {
     return (
-        <main className="container">
-            <p>Welcome!</p>
+        <main className="container home">
+            <h1 className="section-h3">Home</h1>
+            <p>You're logged in!</p>
+            <article className="home-links">
+                <Link to="/profile/editor">Go to Editor's page</Link>
+                <Link to="/profile/admin">Go to Admin's page</Link>
+                <Link to="/profile/lounge">Go to Lounge's page</Link>
+                <Link to="/profile/linkpage">Go to Link's page</Link>
+            </article>
+            <button>
+                Sign out
+            </button>
         </main>
     )
 }
