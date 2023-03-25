2dc7477eb2354d450170106159d06602fab5c8ec
Raheem Kawojue <kawojue08@gmail.com>
2023-03-25 16:56:44 +0100

    remove nav

--- starter/src/components/Layout.tsx
+++ starter/src/components/Layout.tsx
@@ -1,23 +1,8 @@
-import { Outlet, Link } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 
 const Layout: React.FC = () => {
     return (
         <main className="app">
-            <nav className="navbar">
-                <article className="nav-center">
-                    <div>
-                        <p>Homepage</p>
-                    </div>
-                    <ul>
-                        <li>
-                            <Link to="/auth/signup">Signup</Link>
-                        </li>
-                        <li>
-                            <Link to="/auth/login">Login</Link>
-                        </li>
-                    </ul>
-                </article>
-            </nav>
             <Outlet />
         </main>
     )
