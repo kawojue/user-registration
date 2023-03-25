47da736498c43638a105a512281487f03e79c4d6
Raheem Kawojue <kawojue08@gmail.com>
2023-03-25 18:16:43 +0100

    [edit] links page

--- starter/src/components/LinkPage.tsx
+++ starter/src/components/LinkPage.tsx
@@ -1,6 +1,25 @@
+import { Link } from 'react-router-dom'
+
 const LinkPage: React.FC = () => {
     return (
-        <p>LinkPage</p>
+        <main className="container home">
+            <h1 className="section-h3">Links</h1>
+            <section>
+                <h3 className="section-h3">Public</h3>
+                <article className="home-links">
+                    <Link to="/auth/login">Login</Link>
+                    <Link to="/auth/signup">Signup</Link>
+                </article>
+            </section>
+            <section>
+                <h3 className="section-h3">Private</h3>
+                <article className="home-links">
+                    <Link to="/">Home</Link>
+                    <Link to="/editor">Editor's page</Link>
+                    <Link to="/admin">Admin's page</Link>
+                </article>
+            </section>
+        </main>
     )
 }
 
