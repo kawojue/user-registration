7844d13c36d0302374e8ae9aeb76a9fc605b0388
Raheem Kawojue <kawojue08@gmail.com>
2023-03-26 01:28:45 +0100

    ,

--- backend/config/roles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const roles = {
-    Employee: 2384,
-    User: 9823,
-    Admin: 4589
-};
-exports.default = roles;
-//# sourceMappingURL=roles.js.map
--- backend/middlewares/allowedCors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function handleCors(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'www.vercel.com');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-}
-exports.default = handleCors;
-//# sourceMappingURL=allowedCors.js.map
