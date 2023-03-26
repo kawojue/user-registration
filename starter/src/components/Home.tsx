b4ab2a5a5bbb4c33ae1ff12846f7320033561882
Raheem Kawojue <kawojue08@gmail.com>
2023-03-26 12:05:50 +0100

    [add] logout func

--- starter/src/components/Home.tsx
+++ starter/src/components/Home.tsx
@@ -1,7 +1,16 @@
+import axios from '../api/create'
 import { Link } from "react-router-dom"
+import userContext from '../hooks/useContext'
 
 const Home: React.FC = () => {
-    return (
+    const { setAuth }: any = userContext()
+
+    const handleLogout = async () => {
+        await axios.get('/auth/logout')
+        setAuth({})
+    }
+
+    return(
         <main className="container home">
             <h1 className="section-h3">Home</h1>
             <p>You're logged in!</p>
@@ -11,7 +20,7 @@ const Home: React.FC = () => {
                 <Link to="/lounge">Go to Lounge's page</Link>
                 <Link to="/linkpage">Go to Link's page</Link>
             </article>
-            <button>
+            <button onClick={async () => await handleLogout()}>
                 Sign out
             </button>
         </main>
