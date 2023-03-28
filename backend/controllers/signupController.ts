3cf9aa59f171f9afb1d32238d0c7a6744d4a9786
Raheem Kawojue <kawojue08@gmail.com>
2023-03-28 14:06:18 +0100

    refracored

--- backend/controllers/signupController.ts
+++ backend/controllers/signupController.ts
@@ -4,21 +4,17 @@ import { Request, Response } from 'express'
 const asyncHandler = require('express-async-handler')
 
 export const handleSignup = asyncHandler(async (req: Request, res: Response) => {
-    const { email, user, pswd, deviceInfo } = req.body
+    const { email, pswd, deviceInfo }: any = req.body
 
     const mail: string = email?.toLowerCase().trim()
-    const username: string = user?.toLowerCase().trim()
 
-    const existingUser: any = await User.findOne({ username }).exec()
     const existingEmail: any = await User.findOne({ 'mail.email': mail }).exec()
 
-    if (!username || !pswd || !mail) return res.sendStatus(400)
+    if (!pswd || !mail) return res.sendStatus(400)
     if (existingEmail) return res.sendStatus(401)
-    if (existingUser) return res.sendStatus(409)
 
     const hashedPswd: string = await bcrypt.hash(pswd, 12)
     await User.create({
-        username,
         mail: {
             email: mail
         },
