9c8a4f0ccb83963bd31e4824061c25c73912ed80
Raheem Kawojue <kawojue08@gmail.com>
2023-03-28 21:09:29 +0100

    renamed

---
+++ backend/controllers/verifyOTP.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express'
+const asyncHandler = require('express-async-handler')
+
+export const verify = asyncHandler(async (req: Request, res: Response) => {
+    let { userId, otp, totp, date }: any = req.body
+    otp = otp?.trim()
+    userId = userId?.trim().toLowerCase()
+
+    const expiry:number = date + 60 * 60 * 1000
+
+    if (expiry < Date.now()) {
+        return res.status(400).json({
+            message: "OTP Expired."
+        })
+    }
+
+    if (totp !== otp) {
+        return res.status(401).json({
+            message: "Incorrect OTP"
+        })
+    }
+
+    res.status(200).json({
+        verified: true,
+        userId
+    })
+})
