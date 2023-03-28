463a924769af16303ee8b2a0537302c0ef14b0c0
Raheem Kawojue <kawojue08@gmail.com>
2023-03-28 12:53:46 +0100

    .

--- backend/env.d.ts
+++ backend/env.d.ts
@@ -1,14 +1,14 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            DATABASE_URI?: string
-            SECRET_ACCESS_TOKEN?: string
-            SECRET_REFRESH_TOKEN?: string,
             EMAIL: string
-            EMAIL_PSWD: string
             USER_CODE: string
+            EMAIL_PSWD: string
             ADMIN_CODE: string
+            DATABASE_URI: string
             EMPLOYEE_CODE: string
+            SECRET_ACCESS_TOKEN: string
+            SECRET_REFRESH_TOKEN: string
         }
     }
 }
