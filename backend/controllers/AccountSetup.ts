905e933be4c9531ed85b051e5ef10753d73712c3
Raheem Kawojue <kawojue08@gmail.com>
2023-03-28 21:09:41 +0100

    .

---
+++ backend/controllers/AccountSetup.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express'
+const asyncHandler = require('express-async-handler')
+
+const handleAccountSetup = asyncHandler(async (req: Request, res: Response) => {
+
+})
+
+export default handleAccountSetup
