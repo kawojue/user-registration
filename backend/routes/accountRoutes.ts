4185f144c875ab78b7c58d5683b8ef44765384f6
Raheem Kawojue <kawojue08@gmail.com>
2023-03-28 14:39:06 +0100

    [add] acc setup route

--- backend/routes/accountRoutes.ts
+++ backend/routes/accountRoutes.ts
@@ -1,8 +1,11 @@
 import express, { Router } from 'express'
 import passwordRoute from './passwordRoutes'
+import handleAccountSetup from '../controllers/AccountSetup'
 
 const accountRoute: Router = express.Router()
 
 accountRoute.use('/password', passwordRoute)
+
+accountRoute.post('/setup', handleAccountSetup)
 
 export default accountRoute
