7565297c1921fb72b69e3141be1143095bd06152
Raheem Kawojue <kawojue08@gmail.com>
2023-03-29 15:26:02 +0100

    .

--- backend/controllers/AccountSetup.ts
+++ backend/controllers/AccountSetup.ts
@@ -1,8 +1,53 @@
+import User from '../model/userSchema'
 import { Request, Response } from 'express'
 const asyncHandler = require('express-async-handler')
 
-const handleAccountSetup = asyncHandler(async (req: Request, res: Response) => {
+export const handleAccountSetup = asyncHandler(async (req: Request, res: Response) => {
+    let { userId, verifyEmail, vCode, totp, otpDate }: any = req.body
+    userId = userId?.trim()?.toLowerCase()
+    verifyEmail = verifyEmail?.trim()?.toLowerCase()
 
+    const expiry:number = otpDate + 60 * 60 * 1000
+
+    if (!userId) {
+        return res.status(400).json({
+            message: "Invalid Input"
+        })
+    }
+
+    if (!verifyEmail) {
+        return res.status(404).json({
+            message: "Account not found."
+        })
+    }
+
+    if (expiry < Date.now()) {
+        return res.status(400).json({
+            message: "OTP Expired."
+        })
+    }
+
+    if (totp !== vCode) {
+        return res.status(401).json({
+            message: "Incorrect OTP"
+        })
+    }
+
+    const userExists: any = await User.findOne({ username: userId }).exec()
+    const getUser: any = await User.findOne({ 'mail.email': verifyEmail }).exec()
+
+    if (userExists) {
+        return res.status(409).json({
+            message: "Username taken."
+        })
+    }
+
+    getUser.username = userId
+    getUser.mail.isVerified = true
+    await getUser.save()
+
+    res.status(200).json({
+        success: true,
+        message: "Your email has been verified."
+    })
 })
-
-export default handleAccountSetup
