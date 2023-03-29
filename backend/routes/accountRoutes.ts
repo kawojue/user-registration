6ac55cb527a5a3aeeddcfbba7eaabefad968f60d
Raheem Kawojue <kawojue08@gmail.com>
2023-03-29 11:52:20 +0100

    [edit] route

--- backend/routes/accountRoutes.ts
+++ backend/routes/accountRoutes.ts
@@ -1,13 +1,11 @@
 import express, { Router } from 'express'
 import passwordRoute from './passwordRoutes'
-import handleAccountSetup from '../controllers/accountSetup'
-import handleAccountVerification from '../controllers/accountVerify'
+import { handleAccountSetup } from '../controllers/accountSetup'
 
 const accountRoute: Router = express.Router()
 
 accountRoute.use('/password', passwordRoute)
 
 accountRoute.post('/setup', handleAccountSetup)
-accountRoute.post('/verify', handleAccountVerification)
 
 export default accountRoute
