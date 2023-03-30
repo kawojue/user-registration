3ad8dcb916e9a035243a93c2844f7529112b2265
Raheem Kawojue <kawojue08@gmail.com>
2023-03-30 13:51:26 +0100

    refractored

--- backend/controllers/signup.ts
+++ backend/controllers/signup.ts
@@ -1,15 +1,11 @@
 import * as bcrypt from 'bcrypt'
 import User from '../model/userSchema'
 import { Request, Response } from 'express'
-import mailer, { IMailer } from '../config/mailer'
 const asyncHandler = require('express-async-handler')
-import generateOTP, { IGenOTP } from '../config/manageOTP'
 
 export const handleSignup = asyncHandler(async (req: Request, res: Response) => {
     const { email, pswd, deviceInfo }: any = req.body
-
     const mail: string = email?.toLowerCase().trim()
-    const { totpDate, totp }: IGenOTP = generateOTP()
 
     const existingEmail: any = await User.findOne({ 'mail.email': mail }).exec()
 
@@ -22,20 +18,8 @@ export const handleSignup = asyncHandler(async (req: Request, res: Response) => 
             email: mail
         },
         password: hashedPswd,
-        deviceInfo,
-        manageOTP: {
-            totp,
-            totpDate
-        }
+        deviceInfo
     })
-
-    const transportMail: IMailer = {
-        senderName: "Always Appear",
-        to: mail,
-        subject: "Verify your Email.",
-        text: `Code: ${totp}`
-    }
-    await mailer(transportMail)
 
     res.status(201).json({
         success: true,
