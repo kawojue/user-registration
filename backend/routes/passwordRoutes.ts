ce4594a14a3fcf54196edfeb92e71955b1bf42d1
Raheem Kawojue <kawojue08@gmail.com>
2023-03-30 13:52:19 +0100

    [apply] req otp limiter

--- backend/routes/passwordRoutes.ts
+++ backend/routes/passwordRoutes.ts
@@ -1,13 +1,13 @@
 import express, { Router } from 'express'
-import limiter from '../middlewares/limiter'
+import limiter, { otpRequestLimiter } from '../middlewares/limiter'
 import { verify } from '../controllers/verifyOTP'
 import { handleResetPswd } from '../controllers/resetPswd'
 import { handleForgotPswd } from '../controllers/forgotPswd'
 
 const passwordRoute: Router = express.Router()
 
-passwordRoute.post('/reset', handleForgotPswd)
 passwordRoute.post('/verify', limiter, verify)
 passwordRoute.post('/forgotten', handleResetPswd)
+passwordRoute.post('/reset', otpRequestLimiter, handleForgotPswd)
 
 export default passwordRoute
