76aa2b0278216da4a786cf77309363d52f0bb7ce
Raheem Kawojue <kawojue08@gmail.com>
2023-03-31 12:35:34 +0100

    refractored

--- starter/src/hooks/useContext.tsx
+++ starter/src/hooks/useContext.tsx
@@ -21,8 +21,6 @@ export const UserProvider: React.FC<{ children: React.ReactElement }> = ({ child
     const [auth, setAuth] = useState<any>({})
     const [userId, setUserId] = useState<string>("")
 
-    const [user, setUser] = useState<string>('')
-    const [validName, setValidName] = useState<boolean>(false)
     const [userFocus, setUserFocus] = useState<boolean>(false)
 
     const [vCode, setVCode] = useState<string>('')
@@ -41,37 +39,27 @@ export const UserProvider: React.FC<{ children: React.ReactElement }> = ({ child
     const [validConfirm, setValidConfirm] = useState<boolean>(false)
     const [confirmFocus, setConfirmFocus] = useState<boolean>(false)
 
-    const [errMsg, setErrMsg] = useState<string | null>(null)
     const [success, setSuccess] = useState<boolean>(false)
+    const [errMsg, setErrMsg] = useState<string | null>(null)
     
     const [showPswd, setShowPswd] = useState<boolean>(false)
     const [showConfirmPswd, setShowConfirmPswd] = useState<boolean>(false)
 
     useEffect(() => {
-        setErrMsg('')
-    }, [email, user, pswd, confirmPswd])
-
-    useEffect(() => {
         // validate email
-        const resEmail:boolean = EMAIL_REGEX.test(email)
+        const resEmail: boolean = EMAIL_REGEX.test(email)
         setValidEmail(resEmail)
 
-        // validate user
-        const resUser:boolean = USER_REGEX.test(user)
-        setValidName(resUser)
-
         // validate password
-        const resPswd:boolean = PSWD_REGEX.test(pswd)
+        const resPswd: boolean = PSWD_REGEX.test(pswd)
         setValidPswd(resPswd)
         if (pswd) {
-            const confirm:boolean = pswd === confirmPswd
+            const confirm: boolean = pswd === confirmPswd
             setValidConfirm(confirm)
         }
-    }, [email, user, pswd, confirmPswd])
 
-    useEffect(() => {
-        setErrMsg("")
-    }, [email, pswd])
+        setErrMsg('')
+    }, [email, pswd, confirmPswd])
 
     const isValid:boolean = validEmail && validPswd && validConfirm
 
@@ -133,10 +121,10 @@ export const UserProvider: React.FC<{ children: React.ReactElement }> = ({ child
 
     return (
         <Context.Provider value={{
-            Link, errMsg, errRef, user, pswd,
+            Link, errMsg, errRef, pswd,
             confirmPswd, setUserFocus,
-            validName, userFocus, pswdFocus,
-            setPswdFocus, validPswd, setUser,
+            userFocus, pswdFocus,
+            setPswdFocus, validPswd,
             setPswd, setConfirmPswd, validConfirm,
             setConfirmFocus, showPswd, setShowPswd,
             confirmFocus, showConfirmPswd, handleSubmit,
@@ -146,7 +134,7 @@ export const UserProvider: React.FC<{ children: React.ReactElement }> = ({ child
             validEmail, emailRef, auth, EMAIL_REGEX,
             userRef, vCode, setVCode, vCodeFocus,
             setVCodeFocus, verifyEmail, requestOtp,
-            userId, setUserId
+            userId, setUserId, USER_REGEX
         }}>
             {children}
         </Context.Provider>
