9542e74d94c3c9eefb25a4728bc5860f65aa343e
Raheem Kawojue <kawojue08@gmail.com>
2023-04-01 00:09:01 +0100

    refractored

--- starter/src/components/ResetPswd.tsx
+++ starter/src/components/ResetPswd.tsx
@@ -12,15 +12,14 @@ interface IResetPswd {
 
 const ResetPswd: React.FC<IResetPswd> = ({ userId, verified }) => {
     const deviceInfo = detect()
-    const [success, setSuccess] = useState<boolean>(false)
 
     const {
-        confirmFocus, setErrMsg,
-        Link, errMsg, errRef,
+        confirmFocus, ToastContainer,
         pswd, confirmPswd, validPswd,
         setConfirmPswd, setConfirmFocus,
         pswdFocus, setPswdFocus, setPswd,
-        validConfirm, showPswd, setShowPswd
+        validConfirm, showPswd, setShowPswd,
+        showToastMessage
     } = userContext()
 
     const isValid = Boolean(validPswd) && Boolean(validConfirm)
@@ -29,7 +28,7 @@ const ResetPswd: React.FC<IResetPswd> = ({ userId, verified }) => {
         e.preventDefault()
 
         if (!isValid) {
-            setErrMsg("Password does not match.")
+            showToastMessage("warning", "Password does not match.")
             return
         }
 
@@ -42,95 +41,83 @@ const ResetPswd: React.FC<IResetPswd> = ({ userId, verified }) => {
                 },
                 withCredentials: true
             }
-        ).then(res => {
+        ).then((res: any) => {
             setPswd("")
             setConfirmPswd("")
-            setSuccess(res?.data.success)
+            showToastMessage("success", "Password reset successfully.")
         }).catch(err => {
-            setErrMsg(err.response?.data.message)
+            showToastMessage("error", err.response?.data?.message)
         })
     }
 
     return (
         <>
-            {success ? 
-            <article className="user-route">
-                <p className="success">You successfully changed your password.</p>
-                <Link to="/auth/login">Sign in</Link>
-            </article> :
-            <>
-                <div className={`err-container ${errMsg ? 'errMsg' : 'hidden'}`}>
-                    <p ref={errRef} aria-live="assertive">
-                        {errMsg}
-                    </p>
-                </div>
-                <h3 className="section-h3">New Password</h3>
-                <form className="form" method="POST"
-                onSubmit={e => handleSubmit(e)}>
-                    <article className="form-center">
-                        <div className="form-group">
-                            <article className="validity-container">
-                                <label htmlFor='pswd'>
-                                    password:
-                                </label>
-                                <div>
-                                    <FaCheck className={validPswd ? 'valid': 'hidden'} />
-                                    <FaTimes className={validPswd || !pswd ? 'hidden': 'invalid'} />
-                                </div>
-                            </article>
-                            <article className="pswd-container">
-                                <input type={showPswd ? 'text': 'password'} id="pswd" name="pswd"
-                                value={pswd} onChange={e => setPswd(e.target.value)}
-                                onFocus={() => setPswdFocus(true)}
-                                onBlur={() => setPswdFocus(false)}
-                                aria-invalid={validPswd ? "false": "true"} max={89} />
-                                <Button get={showPswd} set={setShowPswd}/>
-                            </article>
-                            <article
-                            className={pswdFocus && pswd && !validPswd ? 'constraint' : 'hidden'}>
-                                <FaInfoCircle />
-                                <p>
-                                    Password must contain: <br />
-                                    At least a Special Character (!@#$&*) <br />
-                                    At least a number (0-9) <br />
-                                    Letters in Lowercase and Uppercase<br />
-                                </p>
-                            </article>
-                        </div>
-                        <div className="form-group">
-                            <article className="validity-container">
-                                <label htmlFor='confirm-pswd'>
-                                    confirm password:
-                                </label>
-                                <div>
-                                    <FaCheck className={validConfirm ? 'valid': 'hidden'} />
-                                    <FaTimes
-                                    className={validConfirm || !confirmPswd ? 'hidden': 'invalid'} />
-                                </div>
-                            </article>
-                            <input id="confirm-pswd" name="confirm_pswd"
-                            type={showPswd ? 'text': 'password'}
-                            value={confirmPswd} onChange={e => setConfirmPswd(e.target.value)}
-                            onFocus={() => setConfirmFocus(true)}
-                            onBlur={() => setConfirmFocus(false)}
-                            aria-invalid={validConfirm ? "false": "true"} />
-                            <article className={confirmFocus && confirmPswd &&
-                            !validConfirm ? 'constraint' : 'hidden'}>
-                                <FaInfoCircle />
-                                <p>
-                                    Password does not match.
-                                </p>
-                            </article>
-                        </div>
-                    </article>
-                    <div className="btn-container">
-                        <button type="submit" className='btn' disabled={!isValid}>
-                            Save
-                        </button>
+            <ToastContainer />
+            <h3 className="section-h3">New Password</h3>
+            <form className="form" method="POST"
+            onSubmit={e => handleSubmit(e)}>
+                <article className="form-center">
+                    <div className="form-group">
+                        <article className="validity-container">
+                            <label htmlFor='pswd'>
+                                password:
+                            </label>
+                            <div>
+                                <FaCheck className={validPswd ? 'valid': 'hidden'} />
+                                <FaTimes className={validPswd || !pswd ? 'hidden': 'invalid'} />
+                            </div>
+                        </article>
+                        <article className="pswd-container">
+                            <input type={showPswd ? 'text': 'password'} id="pswd" name="pswd"
+                            value={pswd} onChange={e => setPswd(e.target.value)}
+                            onFocus={() => setPswdFocus(true)}
+                            onBlur={() => setPswdFocus(false)}
+                            aria-invalid={validPswd ? "false": "true"} max={89} />
+                            <Button get={showPswd} set={setShowPswd}/>
+                        </article>
+                        <article
+                        className={pswdFocus && pswd && !validPswd ? 'constraint' : 'hidden'}>
+                            <FaInfoCircle />
+                            <p>
+                                Password must contain: <br />
+                                At least a Special Character (!@#$&*) <br />
+                                At least a number (0-9) <br />
+                                Letters in Lowercase and Uppercase<br />
+                            </p>
+                        </article>
                     </div>
-                </form>
-            </>
-            }
+                    <div className="form-group">
+                        <article className="validity-container">
+                            <label htmlFor='confirm-pswd'>
+                                confirm password:
+                            </label>
+                            <div>
+                                <FaCheck className={validConfirm ? 'valid': 'hidden'} />
+                                <FaTimes
+                                className={validConfirm || !confirmPswd ? 'hidden': 'invalid'} />
+                            </div>
+                        </article>
+                        <input id="confirm-pswd" name="confirm_pswd"
+                        type={showPswd ? 'text': 'password'}
+                        value={confirmPswd} onChange={e => setConfirmPswd(e.target.value)}
+                        onFocus={() => setConfirmFocus(true)}
+                        onBlur={() => setConfirmFocus(false)}
+                        aria-invalid={validConfirm ? "false": "true"} />
+                        <article className={confirmFocus && confirmPswd &&
+                        !validConfirm ? 'constraint' : 'hidden'}>
+                            <FaInfoCircle />
+                            <p>
+                                Password does not match.
+                            </p>
+                        </article>
+                    </div>
+                </article>
+                <div className="btn-container">
+                    <button type="submit" className='btn' disabled={!isValid}>
+                        Save
+                    </button>
+                </div>
+            </form>
         </>
     )
 }
