5fe10b528a0b51559386a74c2e58bd893dca1b1b
Raheem Kawojue <kawojue08@gmail.com>
2023-04-01 00:09:26 +0100

    import toaster css

--- starter/src/main.tsx
+++ starter/src/main.tsx
@@ -2,6 +2,7 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'react-toastify/dist/ReactToastify.css'
 import { UserProvider } from './hooks/useContext'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
