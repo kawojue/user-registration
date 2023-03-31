d4004b54bf55430e57877a8dd53d1d7d49e857e7
Raheem Kawojue <kawojue08@gmail.com>
2023-04-01 00:04:46 +0100

    refractored

--- starter/src/components/Home.tsx
+++ starter/src/components/Home.tsx
@@ -1,31 +1,41 @@
 import axios from '../api/create'
 import userContext from '../hooks/useContext'
-import { useNavigate, NavigateFunction } from "react-router-dom"
+import { Link, useNavigate, NavigateFunction } from "react-router-dom"
 
 const Home: React.FC = () => {
-    const { setAuth, Link }: any = userContext()
+    const {
+        showToastMessage,
+        ToastContainer, setAuth
+    }: any = userContext()
     const navigate: NavigateFunction = useNavigate()
 
     const handleLogout = async () => {
         await axios.get('/auth/logout')
+        .catch((err: any) => {
+            showToastMessage("error", "Can't logout! Something went wrong.")
+        })
+        localStorage.removeItem("user")
         setAuth({})
         navigate('/linkpage', { replace: true })
     }
 
     return(
-        <main className="container home">
-            <h1 className="section-h3">Home</h1>
-            <p>You're logged in!</p>
-            <article className="home-links">
-                <Link to="/editor">Go to Editor's page</Link>
-                <Link to="/admin">Go to Admin's page</Link>
-                <Link to="/lounge">Go to Lounge's page</Link>
-                <Link to="/linkpage">Go to Link's page</Link>
-            </article>
-            <button onClick={async () => await handleLogout()}>
-                Sign out
-            </button>
-        </main>
+        <>
+            <ToastContainer />
+            <main className="container home">
+                <h1 className="section-h3">Home</h1>
+                <p>You're logged in!</p>
+                <article className="home-links">
+                    <Link to="/editor">Go to Editor's page</Link>
+                    <Link to="/admin">Go to Admin's page</Link>
+                    <Link to="/lounge">Go to Lounge's page</Link>
+                    <Link to="/linkpage">Go to Link's page</Link>
+                </article>
+                <button onClick={async () => await handleLogout()}>
+                    Sign out
+                </button>
+            </main>
+        </>
     )
 }
 
