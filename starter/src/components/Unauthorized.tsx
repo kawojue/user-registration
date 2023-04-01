1c907bb76665d0e2d4481d5b8f1566f06030c6f6
Raheem Kawojue <kawojue08@gmail.com>
2023-04-02 00:45:22 +0100

    add styles

--- starter/src/components/Unauthorized.tsx
+++ starter/src/components/Unauthorized.tsx
@@ -1,6 +1,19 @@
+import { useNavigate, NavigateFunction } from "react-router-dom"
+
 const Unauthorized: React.FC = () => {
+    const navigate: NavigateFunction = useNavigate()
+    const goBack = (): void => navigate(-1)
+
     return (
-        <p>You don't have access to this page.</p>
+        <section className="container">
+            <h3 className="section-h3 mb-9">
+                Unauthorized
+            </h3>
+            <p>You don't have access to this page.</p>
+            <button onClick={goBack} className="mt-5 underline">
+                Go Back
+            </button>
+        </section>
     )
 }
 
