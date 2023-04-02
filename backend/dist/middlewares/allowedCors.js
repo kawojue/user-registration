cf5d4270aa665c35d37158926d8f2c892cd39157
Raheem Kawojue <kawojue08@gmail.com>
2023-04-03 00:38:35 +0100

    .

--- backend/dist/middlewares/allowedCors.js
+++ backend/dist/middlewares/allowedCors.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function handleCors(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'www.vercel.com');
+    res.header("Access-Control-Allow-Origin", 'https://user-registration-kawojue.vercel.app');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 }
