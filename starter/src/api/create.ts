5113da55d7f7c15d57d00287a868785f5484ea0b
Raheem Kawojue <kawojue08@gmail.com>
2023-04-02 16:54:25 +0100

    .

--- starter/src/api/create.ts
+++ starter/src/api/create.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url: string = process.env.NODE_ENV === 'production' ? 'https://user-registration-backend-opal.vercel.app' : 'http://localhost:2003'
+const url: string = process.env.NODE_ENV === 'production' ? 'user-registration-backend-tawny.vercel.app' : 'http://localhost:2003'
 
 export default axios.create({
     baseURL: url
