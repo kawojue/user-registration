f0bc804018639db47176cc20e513ac06116dfe03
Raheem Kawojue <kawojue08@gmail.com>
2023-04-02 16:52:46 +0100

    [edit]

--- backend/controllers/forgotPswd.ts
+++ backend/controllers/forgotPswd.ts
@@ -33,7 +33,7 @@ export const handleForgotPswd = asyncHandler(async (req: Request, res: Response)
     const transportMail: IMailer = {
         senderName: "Always Appear",
         to: mail,
-        subject: "Forgot Password",
+        subject: "Verification/Forgot Password",
         text: `Code: ${totp}`
     }
 
