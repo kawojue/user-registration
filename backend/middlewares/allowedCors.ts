324e97fc6de434ce156bf1cfc271c15bcac8cbf2
Raheem Kawojue <kawojue08@gmail.com>
2023-04-02 17:01:17 +0100

    .

--- backend/middlewares/allowedCors.ts
+++ backend/middlewares/allowedCors.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 export default function handleCors(req: Request, res: Response, next: NextFunction) {
-    res.header("Access-Control-Allow-Origin", 'www.vercel.com');
+    res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 }
