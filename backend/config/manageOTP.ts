7f5e26231738105a309fd89293b662270f6424c6
Raheem Kawojue <kawojue08@gmail.com>
2023-04-04 12:32:03 +0100

    [edit]

--- backend/config/manageOTP.ts
+++ backend/config/manageOTP.ts
@@ -6,7 +6,7 @@ export interface IGenOTP {
 export default function generateOTP(): IGenOTP {
     let totp: string = ''
     const totpDate: number = Date.now()
-    const digits: string = '0123456789'
+    const digits: string = '0918273645'
     const length: number = parseInt('65'[Math.floor(Math.random() * 2)])
     for (let i = 0; i < length; i++) {
         totp += digits[Math.floor(Math.random() * length)]
