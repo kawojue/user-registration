8619f0f2e1b911ea64e75e2f3f2db5c1a273f73e
Raheem Kawojue <kawojue08@gmail.com>
2023-04-04 13:02:03 +0100

    [use] Usenavigate component

--- starter/src/components/RequireUser.tsx
+++ starter/src/components/RequireUser.tsx
@@ -1,16 +1,16 @@
+import UseNavigate from './UseNavigate'
+import { Outlet } from 'react-router-dom'
 import userContext from '../hooks/useContext'
-import { useLocation, Navigate, Outlet, Location } from 'react-router-dom'
 
 
 const RequireUser = ({ allowedRoles }: { allowedRoles?: number[] }) => {
     const { auth }: any = userContext()
-    const location: Location  = useLocation()
 
     return (
         auth?.roles?.find((role: number) => allowedRoles?.includes(role)) ?
         <Outlet/> : auth?.username && auth?.accessToken ?
-        <Navigate to="/unauthorized" state={{ from: location }} replace /> :
-        <Navigate to="/auth/login" state={{ from: location }} replace />
+        <UseNavigate to="/unauthorized" /> :
+        <UseNavigate to="/auth/login" />
     )
 }
 
