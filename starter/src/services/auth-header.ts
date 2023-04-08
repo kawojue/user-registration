0704b539ec3499cffb87bde8d23865780b2b80e4
Raheem Kawojue <kawojue08@gmail.com>
2023-04-08 01:46:38 +0100

    .

--- starter/src/services/auth-header.ts
+++
@@ -1,8 +0,0 @@
-export default function authHeader() {
-    const user: any = JSON.parse(localStorage.getItem("user") as any)
-    if (user && user.accessToken) {
-        return { Authorization: 'Bearer ' + user.accessToken };
-    } else {
-        return {};
-    }
-}
