dc6d01f4211a6e2dd78e2e3173526e075736158b
Raheem Kawojue <kawojue08@gmail.com>
2023-04-09 14:57:09 +0100

    [add] credentials

--- starter/src/api/create.ts
+++ starter/src/api/create.ts
@@ -3,7 +3,8 @@ import axios, { AxiosInstance } from 'axios'
 const BASE_URL: string = process.env.NODE_ENV === 'production' ? 'https://user-registration-backend-tawny.vercel.app' : 'http://localhost:2003'
 
 export default axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+    withCredentials: true,
 })
 
 export const axiosPrivate: AxiosInstance = axios.create({
