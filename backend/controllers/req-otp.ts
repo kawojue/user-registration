bbc8d1f48474afee76a873457268a6a44312a855
Raheem Kawojue <kawojue08@gmail.com>
2023-04-11 14:54:31 +0100

    [use] check mailer

--- backend/controllers/req-otp.ts
+++ backend/controllers/req-otp.ts
@@ -1,5 +1,6 @@
 import User from '../model/userSchema'
 import { Request, Response } from 'express'
+import checkMail from '../config/checkMail'
 import mailer, { IMailer } from '../config/mailer'
 import genOTP, { IGenOTP } from '../config/manageOTP'
 const asyncHandler = require('express-async-handler')
@@ -26,9 +27,17 @@ export const handleReqOTP = asyncHandler(async (req: Request, res: Response) => 
         })
     }
 
-    if (exists.mail.isVerified) {
-        return res.status(401).json({
-            success: false,
+    const isValidEMail: any = await checkMail(mail)
+
+    if (isValidEMail.valid === false) {
+        await exists.deleteOne()
+        return res.status(400).json({
+            message: `${isValidEMail.validators["smtp"].reason}\nYour Account has been deleted.`
+        })
+    }
+
+    if (exists.mail.isVerified && exists.username) {
+        return res.status(409).json({
             message: "Account has already been verified."
         })
     }
