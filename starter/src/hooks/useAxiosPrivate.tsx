6765ad81b7291da3ef4a18da2be37ff37cfd79df
Raheem Kawojue <kawojue08@gmail.com>
2023-04-13 14:27:25 +0100

    [add] interceptors

--- starter/src/hooks/useAxiosPrivate.tsx
+++ starter/src/hooks/useAxiosPrivate.tsx
@@ -2,16 +2,26 @@ import { useEffect } from 'react'
 import userContext from './useContext'
 import { axiosPrivate } from '../api/create'
 import useRefreshToken from './useRefreshToken'
-import { AxiosResponse, AxiosInstance, InternalAxiosRequestConfig } from 'axios'
+import { AxiosResponse, AxiosInstance } from 'axios'
 
 const useAxiosPrivate = (): AxiosInstance => {
     const refresh = useRefreshToken()
     const { auth }: any = userContext()
 
     useEffect(() => {
+        const reqIntercept: number = axiosPrivate.interceptors.request.use(
+            (config: any) => {
+                if (!config.headers["Authorization"]) {
+                    config.headers["Authorization"] = `Bearer ${auth?.accessToken}`
+                }
+                return config
+            },
+            (error: any) => Promise.reject(error)
+        )
+
         const resIntercept: number = axiosPrivate.interceptors.response.use(
             (response: AxiosResponse<any, any>) => response,
-            async (error) => {
+            async (error: any) => {
                 const prevReq = error?.config
                 if (error?.response?.status === 403 && !prevReq?.sent) {
                     prevReq.sent = true
@@ -19,10 +29,12 @@ const useAxiosPrivate = (): AxiosInstance => {
                     prevReq.headers["Authorization"] = `Bearer ${newAccessToken}`
                     return axiosPrivate(prevReq)
                 }
+                return Promise.reject(error)
             }
         )
 
         return () => {
+            axiosPrivate.interceptors.request.eject(reqIntercept)
             axiosPrivate.interceptors.response.eject(resIntercept)
         }
     }, [refresh, auth])
