b180e00417b14a6f546c358fa5cd40b5e4c63655
Raheem Muyiwa <kawojue08@gmail.com>
2023-04-14 12:40:39 +0100

    Delete allowedCors.ts

--- backend/middlewares/allowedCors.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express'
-
-export default function handleCors(req: Request, res: Response, next: NextFunction) {
-    res.header("Access-Control-Allow-Origin", 'https://vercel.com/');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-}
